254ec70ee2c3875c7d8ee33eba3b37383c55d2eb
Julian Hansen <jshman1435@gmail.com>
2016-04-06 22:18:11 +0000

    update

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,4 @@
 class Micropost < ActiveRecord::Base
-belongs_to :user
-validates :content, length: { maximum: 140 },
-presence: true
-
+    belongs_to :user
+    validates :content, length: { maximum: 140 }, presence: true
 end
